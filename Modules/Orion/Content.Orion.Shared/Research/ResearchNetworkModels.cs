e648bd3d548704d6629f6ea4d9227279017b5b33
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2026-06-09 13:07:10 +0300

    feat(rnd): add shared network log model

--- Modules/Orion/Content.Orion.Shared/Research/ResearchNetworkModels.cs
+++ Modules/Orion/Content.Orion.Shared/Research/ResearchNetworkModels.cs
@@ -1,3 +1,4 @@
+using Robust.Shared.GameObjects;
 using Robust.Shared.Serialization;
 
 namespace Content.Orion.Shared.Research;
@@ -10,4 +11,20 @@ public partial record struct ResearchPointAmount
 
     [DataField]
     public int Amount;
+}
+
+[DataDefinition, Serializable, NetSerializable]
+public partial record struct ResearchLogEntry
+{
+    [DataField]
+    public TimeSpan Timestamp;
+
+    [DataField(required: true)]
+    public string Category;
+
+    [DataField(required: true)]
+    public string Message;
+
+    [DataField]
+    public NetEntity? Actor;
 }
